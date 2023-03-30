b9f7c0584d6bb5b0e210cb7a4343254c851dd873
Samuel Clements <samuelclements@Samuels-MacBook-Pro.local>
2023-03-30 11:28:12 +0100

    adding files to host API

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,18 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 const ENV = process.env.NODE_ENV || "development";
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
+
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -13,3 +25,4 @@ if (!process.env.PGDATABASE) {
 }
 
 module.exports = new Pool();
+module.exports = new Pool(config);
